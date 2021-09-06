f2d3a1f7b7512d15e58cafd009ae4dbd44af0375
thanhnew2001 <thanh.hispvietnam@gmail.com>
2021-09-06 11:51:34 +0700

    Create Button.js

---
+++ Button.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Text, View, Button } from 'react-native';
+
+const YourApp = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+      
+      <Text>goodbye</Text>
+      
+      <Text>goodfriend</Text>
+      
+      <Text>goodgirl</Text>
+      
+      <Text>goodboy</Text>
+      
+      <Button title='OK'/>
+      <Button  title='hi'/>
+      <Button  title='hello'/>
+      <Button  title='sorry'/>
+      <Button  title='like'/> 
+    </View>
+  );
+}
+
+export default YourApp;
