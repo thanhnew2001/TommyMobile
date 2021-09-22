d17d855a281d9bedf2156b9404c5f9d73abe4dbe
thanhnew2001 <thanh.hispvietnam@gmail.com>
2021-09-22 10:11:39 +0700

    Create emojibutton.js

---
+++ emojibutton.js
@@ -0,0 +1,300 @@
+import React, {useState} from 'react';
+import { Text, View, Button } from 'react-native';
+
+const YourApp = () => {
+  const [cat, setCat] = useState('')
+
+   return (
+    <View style={{padding: 10}}>
+    
+      <Text style={{fontSize:50}}>{cat}</Text>
+      
+      <Button title='💎' 
+      onPress={()=>setCat('💎')}
+      />
+      
+      
+      
+      
+      
+      
+      
+      
+      <Button title='💣' 
+      onPress={()=>setCat('💣')}
+      />
+       
+      
+      
+      
+      
+      
+      
+      
+      
+       <Button title='💗 ' 
+      onPress={()=>setCat('💗 ')}
+      />
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+       <Button title='🌋' 
+      onPress={()=>setCat('🌋')}
+      />
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+       <Button title='✨' 
+      onPress={()=>setCat('✨')}
+      />
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+       <Button title='🌪' 
+      onPress={()=>setCat('🌪')}
+      />
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+       <Button title='☄️' 
+      onPress={()=>setCat('☄️')}
+      />
+      
+      
+      
+      
+      
+      
+      
+      
+      
+    <Button title='🔥' 
+      onPress={()=>setCat('🔥')}
+      />
+        
+      
+      
+      
+      
+      
+      
+      
+      <Button title='🌊' 
+      onPress={()=>setCat('🌊')}
+      />
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      <Button title='🌫' 
+      onPress={()=>setCat('🌫')}
+      />
+      
+      
+      
+      
+      
+      
+      
+      <Button title='🐉' 
+      onPress={()=>setCat('🐉')}
+      />
+      
+      
+      
+      
+      
+      
+      
+     <Button title='🕸' 
+      onPress={()=>setCat('🕸')}
+      /> 
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      <Button title='⚡️' 
+      onPress={()=>setCat('⚡️')}
+      />  
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+       <Button title='🥠' 
+      onPress={()=>setCat('🥠')}
+      /> 
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+       <Button title='🪛' 
+      onPress={()=>setCat('🪛')}
+      /> 
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+       <Button title='🚪' 
+      onPress={()=>setCat('🚪')}
+      /> 
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+       <Button title='🏋🏽' 
+      onPress={()=>setCat('🏋🏽')}
+      /> 
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+       <Button title='🌑' 
+      onPress={()=>setCat('🌑')}
+      /> 
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+       <Button title='🦅' 
+      onPress={()=>setCat('🦅')}
+      />
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+     <Button title='🐾' 
+      onPress={()=>setCat('🐾')}
+      /> 
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+    <Button title='🕊' 
+      onPress={()=>setCat('🕊')}
+      />   
+      
+      
+      
+      
+      
+      
+       
+      
+      
+      
+      <Button title='🧪' 
+      onPress={()=>setCat('🧪')}
+      /> 
+      
+      
+      
+      
+
+    </View>
+  );
+
+}
+
+export default YourApp;
